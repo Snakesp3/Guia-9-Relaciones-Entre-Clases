e572986ab11a82df6510c6b747a4759710b6abbf
Sebastian G <resgirolminipcparts2014@gmail.com>
2022-09-24 09:27:53 -0300

    Guia 9 eje MAO1

--- Ejercicios MAO/Guia 9 Relaciones entre clases MAO01/src/Persona/Main.java
+++ Ejercicios MAO/Guia 9 Relaciones entre clases MAO01/src/Persona/Main.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Persona;
 
-/**
- *
- * @author Snakesp2
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    /*Realiza un programa en donde una clase Persona tenga como atributo nombre, apellido y un
+objeto de clase Dni. La clase Dni tendrá como atributos la serie (carácter) y número. Prueba
+acceder luego a los atributos del dni de la persona creando objetos y jugando desde el main.*/
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Persona persona = new Persona();
+        dni p1 = new dni();
+        persona.setNombre("Sebastian");
+        persona.setApellido("Girolmini");
+        p1.setSerie("A");
+        p1.setNumero(32668773);
+        System.out.println("Los datos de la persona son:\n" + "Nombre y Apellido: " + persona.getNombre() + " " + persona.getApellido()
+                + "\nNumero de Serie: " + p1.getSerie() + "\nNumero DNI" + p1.getNumero());
+
     }
-    
+
 }
---
+++ Ejercicios MAO/Guia 9 Relaciones entre clases MAO01/src/Persona/Persona.java
@@ -0,0 +1,45 @@
+
+package Persona;
+
+/*Realiza un programa en donde una clase Persona tenga como atributo nombre, apellido y un
+objeto de clase Dni. La clase Dni tendrá como atributos la serie (carácter) y número. Prueba
+acceder luego a los atributos del dni de la persona creando objetos y jugando desde el main.*/
+public class Persona {
+   
+    private String nombre;
+    private String apellido;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Entidad{" + "nombre=" + nombre + ", apellido=" + apellido + '}';
+    }
+    
+    
+    
+    
+    
+}
